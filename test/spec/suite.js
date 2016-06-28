aa05ed555fdbec999979a5f6490bbc42592ff20a
Sandro Pasquali <sandro.pasquali@twintechs.com>
2016-06-28 15:56:26 -0400

    refactor, added tests

--- test/spec/suite.js
+++ test/spec/suite.js
@@ -9,5 +9,27 @@ let inst = require('../../lib')({
 
 module.exports = function(test, Promise) {
 
+    return inst
+
+    .set('foo', {
+        bar: 'baz'
+    })
+    .then(() => inst.get('foo'))
+    .then(val => {
+        test.ok(_.isPlainObject(val), 'Correctly set and fetched Object from cache')
+    })
+    .catch(err => {
+        test.fail('Did not correctly set and get Object from cache');
+    })
+
+    .then(() => inst.set('foo', 'baz'))
+    .then(() => inst.get('foo'))
+    .then(val => {
+        test.ok(_.isString(val), 'Correctly set and fetched String from cache')
+    })
+    .catch(err => {
+        test.fail('Did not correctly set and get String from cache');
+    })
+
     return Promise.resolve('ok')
 };
